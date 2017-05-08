68db732f7d03797d9b4da43454576130c1f0a093
David A. Lareo <dalareo@gmail.com>
2017-05-08 20:00:37 +0200

    changed path to apps2

--- templates/content/index.php
+++ templates/content/index.php
@@ -6,7 +6,7 @@
     </div>
     <div class="col-md-4 col-xs-4">
       <div class="box">
-       <a href="http://openedx.educaas.io"><img src="/apps/educaasdashboard/img/logo-edx.png"></a>
+       <a href="http://openedx.educaas.io"><img src="/apps2/educaasdashboard/img/logo-edx.png"></a>
       </div>
     </div>
     <div class="col-md-4 col-xs-4">
@@ -17,34 +17,34 @@
   <div class="row">
     <div class="col-md-4 col-xs-4">
       <div class="box">
-        <a href="https://git.educaas.io"><img src="/apps/educaasdashboard/img/gogs.png"></a>
+        <a href="https://git.educaas.io"><img src="/apps2/educaasdashboard/img/gogs.png"></a>
       </div>
     </div>
     <div class="col-md-4 col-xs-4">
       <div class="box">
-        <a href="https://canvas.educaas.io"><img src="/apps/educaasdashboard/img/canvas.svg"></a>
+        <a href="https://canvas.educaas.io"><img src="/apps2/educaasdashboard/img/canvas.svg"></a>
       </div>
     </div>
     <div class="col-md-4 col-xs-4">
       <div class="box">
-        <a href="https://chat.educaas.io"><img src="/apps/educaasdashboard/img/rocket-chat.svg"></a>
+        <a href="https://chat.educaas.io"><img src="/apps2/educaasdashboard/img/rocket-chat.svg"></a>
       </div>
     </div>
   </div>
   <div class="row">
     <div class="col-md-4 col-xs-4">
       <div class="box">
-        <a href="https://agile.educaas.io"><img src="/apps/educaasdashboard/img/wekan.svg"></a>
+        <a href="https://agile.educaas.io"><img src="/apps2/educaasdashboard/img/wekan.svg"></a>
       </div>
     </div>
     <div class="col-md-4 col-xs-4">
       <div class="box">
-        <a href="https://videochat.educaas.io"><img src="/apps/educaasdashboard/img/hublin.svg"></a>
+        <a href="https://videochat.educaas.io"><img src="/apps2/educaasdashboard/img/hublin.svg"></a>
       </div>
     </div>
     <div class="col-md-4 col-xs-4">
       <div class="box">
-        <a href="https://blog.educaas.io"><img src="/apps/educaasdashboard/img/wordpress.svg"></a>
+        <a href="https://blog.educaas.io"><img src="/apps2/educaasdashboard/img/wordpress.svg"></a>
       </div>
     </div>
   </div>
