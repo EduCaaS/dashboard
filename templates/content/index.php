354b157997bef2bcd380d2813f5fcb6facd0baea
David A. Lareo <dalareo@gmail.com>
2017-05-09 16:59:23 +0200

    modify path to apps2

--- templates/content/index.php
+++ templates/content/index.php
@@ -6,7 +6,7 @@
     </div>
     <div class="col-md-4">
       <div class="box">
-       <a href="http://openedx.educaas.io"><img src="/apps/educaasdashboard/img/logo-edx.png"></a>
+       <a href="http://openedx.educaas.io"><img src="/apps2/educaasdashboard/img/logo-edx.png"></a>
         <h4 class="app-title">Cursos</h4>
       </div>
     </div>
@@ -18,19 +18,19 @@
   <div class="row">
     <div class="col-md-4">
       <div class="box">
-        <a href="https://blog.educaas.io"><img src="/apps/educaasdashboard/img/wordpress.svg"></a>
+        <a href="https://blog.educaas.io"><img src="/apps2/educaasdashboard/img/wordpress.svg"></a>
         <h4 class="app-title">Blog</h4>
       </div>
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://canvas.educaas.io"><img src="/apps/educaasdashboard/img/canvas.svg"></a>
+        <a href="https://canvas.educaas.io"><img src="/apps2/educaasdashboard/img/canvas.svg"></a>
         <h4 class="app-title">Creative Canvas</h4>
       </div>
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://chat.educaas.io"><img src="/apps/educaasdashboard/img/rocket-chat.svg"></a>
+        <a href="https://chat.educaas.io"><img src="/apps2/educaasdashboard/img/rocket-chat.svg"></a>
         <h4 class="app-title">Chat</h4>
       </div>
     </div>
@@ -38,19 +38,19 @@
   <div class="row">
     <div class="col-md-4">
       <div class="box">
-        <a href="https://agile.educaas.io"><img src="/apps/educaasdashboard/img/wekan.svg"></a>
+        <a href="https://agile.educaas.io"><img src="/apps2/educaasdashboard/img/wekan.svg"></a>
         <h4 class="app-title">Gestión de proyectos</h4>
       </div>
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://videochat.educaas.io"><img src="/apps/educaasdashboard/img/hublin.svg"></a>
+        <a href="https://videochat.educaas.io"><img src="/apps2/educaasdashboard/img/hublin.svg"></a>
         <h4 class="app-title">Videochat</h4>
       </div>
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://git.educaas.io"><img src="/apps/educaasdashboard/img/gogs.png"></a>
+        <a href="https://git.educaas.io"><img src="/apps2/educaasdashboard/img/gogs.png"></a>
         <h4 class="app-title">Repositorio</h4>
       </div>
     </div>
