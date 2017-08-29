0f1b20c2f696e707368df6ff3b6af235dba7fdcd
David A. Lareo <dalareo@gmail.com>
2017-08-29 18:23:40 +0200

    minor css changes background color and h4

--- templates/content/index.php
+++ templates/content/index.php
@@ -3,19 +3,19 @@
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://blog.educaas.io"><img src="/custom_apps/educaasdashboard/img/wordpress.svg"></a>
-        <h4 class="app-title">Blog</h4>
+        <h2 class="app-title">Blog</h2>
       </div>
     </div>
     <div class="col span_1_of_3">
       <div class="box">
         <a href="http://openedx.educaas.io"><img src="/custom_apps/educaasdashboard/img/logo-edx.png"></a>
-        <h4 class="app-title">Cursos</h4>
+        <h2 class="app-title">Cursos</h2>
       </div>
     </div>
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://chat.educaas.io"><img src="/custom_apps/educaasdashboard/img/rocket-chat.svg"></a>
-        <h4 class="app-title">Chat</h4>
+        <h2 class="app-title">Chat</h2>
       </div>
     </div>
   </div>
@@ -23,19 +23,19 @@
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://slides.educaas.io"><img src="/custom_apps/educaasdashboard/img/slides.jpg"></a>
-        <h4 class="app-title">Presentaciones</h4>
+        <h2 class="app-title">Presentaciones</h2>
       </div>
     </div>
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://canvas.educaas.io"><img src="/custom_apps/educaasdashboard/img/canvas.svg"></a>
-        <h4 class="app-title">Creative Canvas</h4>
+        <h2 class="app-title">Creative Canvas</h2>
       </div>
     </div>
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://draw.educaas.io"><img src="/custom_apps/educaasdashboard/img/diagramas.jpg"></a>
-        <h4 class="app-title">Diagramas</h4>
+        <h2 class="app-title">Diagramas</h2>
       </div>
     </div>
   </div>
@@ -43,7 +43,7 @@
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://agile.educaas.io"><img src="/custom_apps/educaasdashboard/img/wekan.svg"></a>
-        <h4 class="app-title">Gestión de proyectos</h4>
+        <h2 class="app-title">Gestión de proyectos</h2>
       </div>
     </div>
     <div class="col span_1_of_3">
@@ -53,7 +53,7 @@
     <div class="col span_1_of_3">
       <div class="box">
         <a href="https://git.educaas.io"><img src="/custom_apps/educaasdashboard/img/gogs.png"></a>
-        <h4 class="app-title">Repositorio</h4>
+        <h2 class="app-title">Repositorio</h2>
       </div>
     </div>
   </div>
